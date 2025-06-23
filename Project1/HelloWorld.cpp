5524a3222f1d3846bfb1bf796ee5b8464311c05c
xingyuZHOU <3391200591@qq.com>
2025-06-23 23:12:59 +0800

    删除project1

--- Project1/HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-	cout<<"good"<<endl;
-	cout << "Hello, World!" << endl;
-	cout << "This is a simple C++ program." << endl;
-	return 0;
-	
-}	
