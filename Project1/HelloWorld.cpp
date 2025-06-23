a95e080e86a1fd79bc49a1a55825a93723028d54
xingyuZHOU <3391200591@qq.com>
2025-06-23 22:45:31 +0800

    第二次提交

--- Project1/HelloWorld.cpp
+++ Project1/HelloWorld.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
+	cout<<"good"<<endl;
 	cout << "Hello, World!" << endl;
+	cout << "This is a simple C++ program." << endl;
 	return 0;
+	
 }	
