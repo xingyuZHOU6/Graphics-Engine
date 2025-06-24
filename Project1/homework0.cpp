f39a844bc064e0c784aea095e5255d8cb7b0c600
xingyuZHOU <3391200591@qq.com>
2025-06-24 21:31:19 +0800

    作业0提交

--- Project1/homework0.cpp
+++ Project1/homework0.cpp
@@ -1,24 +1,35 @@
-#include<iostream>
+﻿#include<iostream>
 #include<Eigen>
 #include<cmath>
 #define PI 3.14159265358979323846
 using namespace std;
+/*
+	作业要求：给定一个点 P=(2,1), 将该点绕原点先逆时针旋转 45 ◦ ，
+	再平移 (1,2), 计算出变换后点的坐标（要求用齐次坐标进行计算）。
+
+	提示：齐次坐标的变换矩阵为：cos θ  -sin θ  tx 
+								sin θ   cos θ  ty
+								0       0      1
+		其中，tx 和 ty 分别为平移的 x 和 y 分量。
+
+*/
+
 int main()
 {
-	// ��ʽģ�嶨�� (3��1��)
+	// 显式模板定义 (3行1列)
 	Eigen::Matrix<double, 3, 1> mat;
 
-	// ��ʼ��������
+	// 初始化方法：
 	mat <<2,1,1;
 
-	//�任����
+	//变换矩阵
 	Eigen::Matrix3d rotation_matrix;
-	double angle = 45 * PI / 180; // �Ƕ�ת��Ϊ����	
+	double angle = 45 * PI / 180; // 角度转换为弧度	
 	rotation_matrix << cos(angle), -sin(angle), 1,
 					  sin(angle), cos(angle), 2,
 		              0, 0, 1;
-	// ������
+	// 计算结果
 	Eigen::Matrix<double, 3, 1> result = rotation_matrix * mat;
-	// ������
+	// 输出结果
 	cout << "Result of the transformation:\n" << result << endl;
 }
